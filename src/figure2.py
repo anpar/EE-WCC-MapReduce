8b1b95a9b3d023fd97873b55b1fb0065e1113430
anpar <paris.antoine.paris@gmail.com>
2019-02-15 15:26:08 +0100

    Add license terms in each file

--- src/figure2.py
+++ src/figure2.py
@@ -1,3 +1,34 @@
+"""
+MIT License
+
+Copyright (c) 2019 Université Catholique de Louvain (UCLouvain)
+
+The software provided allows to reproduce the results presented in the
+research paper "Energy-Efficient Edge-Facilitated Wireless Collaborative
+Computing using Map-Reduce" by Antoine Paris, Hamed Mirghasemi, Ivan Stupia
+and Luc Vandendorpe from ICTEAM/ELEN/CoSy (UCLouvain).
+
+Contact: antoine.paris@uclouvain.be
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import matplotlib.pyplot as plt
 
 #from matplotlib2tikz import save as tikz_save
